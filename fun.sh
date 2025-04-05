c96e7b6c3cc5c9cca5553d53ec8763322745f82d
LION TUNNEL <mrdoe0435@gmail.com>
2025-04-05 11:43:52 +0700

    Update fun.sh

--- fun.sh
+++ fun.sh
@@ -319,7 +319,7 @@ curl "ipinfo.io/city?token=7a814b6263b02c" > /root/.city
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
-izinsc="https://raw.githubusercontent.com/Eritsuu/joy/main/daftar"
+izinsc="https://raw.githubusercontent.com/liontunnel/joy/main/daftar"
 # USERNAME
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}')
